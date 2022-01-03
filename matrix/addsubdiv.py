419d48f769f1fc8351b614768687969c87da57a7
VismayaN <79937728+VismayaN@users.noreply.github.com>
2022-01-03 10:21:04 +0530

    prgm5

---
+++ matrix/addsubdiv.py
@@ -0,0 +1,7 @@
+import numpy as np
+a=np.array([[2,6,9],[3,7,5]])
+b=np.array([[3,2,1],[4,6,2]])
+print("first matrix is..\n",a,"\nsecond matrix is..\n",b,"\n")
+print("sum of an array..\n",np.array(a)+np.array(b),"\n")
+print("difference of an array..\n",np.array(a)-np.array(b),"\n")
+print("division of an array..\n",np.array(a)/np.array(b),"\n")
